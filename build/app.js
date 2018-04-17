84628713ac50bc8eba25a1feae031d04c563ddb9
刘建川 <liujianchuan@ZBMAC-C02VM6NEH.local>
2018-04-17 18:48:01 +0800

    step1: chunk

--- build/app.js
+++ build/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world!');
-})
+// app.get('/', function(req, res){
+//     res.send('hello world!');
+// })
+app.use(express.static('../dist/assets'));
 
 const server = app.listen(3000, function(){
-    let host = server.address().address;
+    let h = server.address();
+    console.log(h);
     let port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', h, port);
 })
